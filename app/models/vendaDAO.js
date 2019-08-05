8d9e19284c425eaee44ba19d00124093e9648f22
Your Name <you@example.com>
2019-08-05 15:42:40 -0300

    cadastrando venda

--- app/models/vendaDAO.js
+++ app/models/vendaDAO.js
@@ -11,44 +11,52 @@ class VendaDao {
                         if (err != null) {
                             reject(err)
                         } else {
-                            var produto = result;
-                            mongoCliente.collection('clientes', (err, collection) => {
-                                collection.update({
-                                    _id: { $eq: objectId(dadosCompra.idCliente) }
-                                }, {
-                                        $push: {
-                                            compras: {
-                                                qtd: produto.qtd,
-                                                marca: produto.marca,
-                                                modelo: produto.modelo,
-                                                tamanho: produto.tamanho,
-                                                cor: produto.tamanho,
-                                                preco: produto.preco,
-                                                total: produto.preco * dadosCompra.qtd
-                                            }
-                                        }
-                                    }, (err, result) => {
-                                        if (err != null) {
-                                            reject(err);
-                                        } else {
-                                            mongoCliente.collection('produtos', (err, collection) => {
-                                                collection.update({
-                                                    _id: objectId(dadosCompra.idProduto)
-                                                }, {
-                                                        $set: { qtd: produto.qtd - dadosCompra.qtd }
-                                                    }, (err, result) => {
-                                                        (err != null) ? reject(err) : resolve({ msg: "Venda cadastrada com sucesso !", status: "ok" })
-                                                    })
-                                            })
-                                            //  mongoCliente.close();
-                                        }
-                                    })
-                                //mongoCliente.close();
+                            result.toArray((err, result) => {
+                                if (err != null) {
+                                    reject(err);
+                                } else {
+                                    var produto = result;
+                                    if (produto[0].qtd < dadosCompra.qtd) {
+                                        reject({ msg: 'Nao há produtos suficientes', status: 'err' });
+                                    } else {
+                                        mongoCliente.collection('clientes', (err, collection) => {
+                                            collection.update({
+                                                _id: { $eq: objectId(dadosCompra.idCliente) }
+                                            }, {
+                                                    $push: {
+                                                        compras: {
+                                                            qtd: parseInt(dadosCompra.qtd),
+                                                            marca: produto[0].marca,
+                                                            modelo: produto[0].modelo,
+                                                            tamanho: parseInt(produto[0].tamanho),
+                                                            cor: produto[0].cor,
+                                                            preco: parseFloat(produto[0].preco),
+                                                            total: produto[0].preco * dadosCompra.qtd
+                                                        }
+                                                    }
+                                                }, (err, result) => {
+                                                    if (err != null) {
+                                                        reject(err);
+                                                    } else {
+                                                        mongoCliente.collection('produtos', (err, collection) => {
+                                                            collection.update({
+                                                                _id: objectId(dadosCompra.idProduto)
+                                                            }, {
+                                                                    $set: { qtd: parseInt(produto[0].qtd - dadosCompra.qtd) }
+                                                                }, (err, result) => {
+                                                                    (err != null) ? reject(err) : resolve({ msg: "Venda cadastrada com sucesso !", status: "ok" })
+                                                                })
+                                                        })
+                                                    }
+                                                })
+                                        })
+                                    }
+                                }
                             })
                         }
                     })
-                    mongoCliente.close();
                 })
+                //mongoCliente.close();
             })
         })
         return promiseCadastrarVenda;
